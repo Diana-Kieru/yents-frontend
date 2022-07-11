e5c3f26efa24d92a79bc62321d62c0537f6bee50
Maurice Nganga <61679467+moryno@users.noreply.github.com>
2022-07-11 13:41:08 +0300

    Create a navbar with navigation items

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,28 +1,62 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
-import './Navbar.css';
+const Container = styled.div`
+    height: 3.75rem;
+    width: 100vw;
+    background-color: blue;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+`;
+
+const Wrapper = styled.div`
+   padding: 1rem 2rem;
+   display: flex;
+   aligh-items: center;
+   justify-content: space-evenly;
+   width: 50%;
+ 
+`;
+
+const MenuItem = styled.div`
+  cursor: pointer;
+  font-size: 1rem;
+  margin-right: 1rem;
+`;
+
+const Button = styled.button``;
 
 const Navbar = () => {
   return (
-    <div>
-      <Link to='/'>
-        <div className="logo__container">
-            <img src="" alt="" />
-        </div>
-      </Link>
+    <Container>
+      <Wrapper>
+        <MenuItem> Whitepaper </MenuItem>
+        <MenuItem> Roadmap </MenuItem>
+        <MenuItem> Team </MenuItem>
+        <MenuItem> Contact us </MenuItem>
+        <MenuItem> <Button>Sign up</Button></MenuItem>
+      </Wrapper>
+    </Container>
+    // <div>
+    //   <Link to='/'>
+    //     <div className="logo__container">
+    //         <img src="" alt="" />
+    //     </div>
+    //   </Link>
         
-        <nav>
+    //     <nav>
             
             
-        </nav>
-        <Link to="/login" style={{textDecoration: "none"}}>
-          <button>Log in</button>
-        </Link>
-        <Link to="/signup" style={{textDecoration: "none"}}>
-          <button>Sign up</button>
-        </Link>
-    </div>
+    //     </nav>
+    //     <Link to="/login" style={{textDecoration: "none"}}>
+    //       <button>Log in</button>
+    //     </Link>
+    //     <Link to="/signup" style={{textDecoration: "none"}}>
+    //       <button>Sign up</button>
+    //     </Link>
+    // </div>
   )
 }
 
